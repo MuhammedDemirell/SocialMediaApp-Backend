d2f079c1af286fdd473e7af670ad052f062b70d2
Muhammed Demirel <muhammed_demirell@hotmail.com>
2023-08-23 20:20:34 +0300

    created getUserById and deleteUser test

--- social-media/src/test/java/com/demirel/socialmedia/controller/UserControllerTest.java
+++ social-media/src/test/java/com/demirel/socialmedia/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.demirel.socialmedia.controller;
 
+import com.demirel.socialmedia.model.dto.UserDto;
 import com.demirel.socialmedia.model.entity.User;
 import com.demirel.socialmedia.model.request.CreateUserRequest;
 import com.demirel.socialmedia.repository.UserRepository;
@@ -7,13 +8,17 @@ import com.demirel.socialmedia.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ActiveProfiles("test")
 class UserControllerTest {
@@ -21,30 +26,27 @@ class UserControllerTest {
     @InjectMocks
     private UserService userService;
 
+    @Autowired
+    MockMvc mockMvc;
+
     @Mock
     private UserRepository userRepository;
 
     @Test
     void getAllUsers() {
-        // UserRepository'yi sahte bir nesneyle değiştiriyoruz
         UserRepository userRepository = mock(UserRepository.class);
 
-        // Kaydedilecek kullanıcı listesini oluşturuyoruz
         List<User> usersToSave = new ArrayList<>();
         usersToSave.add(new User(1L, "muhammed", "123", null, null, null, 1));
         usersToSave.add(new User(2L, "melih", "321", null, null, null, 1));
 
-        // saveAll metodunun davranışını tanımlıyoruz
         when(userRepository.saveAll(usersToSave)).thenReturn(usersToSave);
 
-        // findAll metodunun davranışını tanımlıyoruz
         when(userRepository.findAll()).thenReturn(usersToSave);
 
-        // Test işlemlerine devam edebiliriz
         List<User> savedUsers = userRepository.saveAll(usersToSave);
         List<User> retrievedUsers = userRepository.findAll();
 
-        // Kaydedilen ve alınan kullanıcı listelerinin beklenen kullanıcı listesiyle eşleşip eşleşmediğini kontrol ediyoruz
         assertEquals(usersToSave, savedUsers);
         assertEquals(usersToSave, retrievedUsers);
 
@@ -62,13 +64,23 @@ class UserControllerTest {
 
     @Test
     void getUserById() {
+        Long mockId = 1L;
+        when(userRepository.existsById(mockId)).thenReturn(true);
+        assertTrue(userRepository.existsById(mockId));
+        verify(userRepository, times(1)).existsById(mockId);
+
     }
 
     @Test
     void updateUser() {
+
     }
 
     @Test
     void deleteUser() {
+        long mockId = 1L;
+        userRepository.deleteById(mockId);
+        verify(userRepository, times(1)).deleteById(mockId);
+
     }
 }
